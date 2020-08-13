cd2a247af934027d4909346f3ebecef7ffe5cf7e
dawoodiddris <dawoodiddris@gmail.com>
2020-08-13 06:46:39 -0700

    added functions file

---
+++ explorefunction.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 13 06:28:49 2020
+
+@author: GTL-NIL-001
+"""
+
+def greet():
+    print("Hello world")
+    
+greet()   
+
+
+def add(first, second):
+    total = (first + second)
+    print(total)
+
+add(4, 6)
+    
